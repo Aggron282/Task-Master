2fd311c6a7836defedfc7058e2c40b308c6b2453
Aggron282 <marcokhodr1@gmail.com>
2025-04-23 21:43:12 -0700

    s

---
+++ util/db.js
@@ -0,0 +1,4 @@
+const mongodb_connection = "mongodb+srv://marcokhodr116:aggron828@cluster0.81dhu2y.mongodb.net/task_master?retryWrites=true&w=majority&appName=Cluster0"
+
+
+module.exports.mongodb_connection = mongodb_connection;
